8cd025498fce40c6ad4b5305a69e9db048fec785
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-23 20:00:56 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class test2{
+    public static void main(String args[])
+    {
+        Scanner kishore = new Scanner(System.in);
+        String name = kishore.nextLine();
+        int age = kishore.nextInt();
+
+        System.out.println("my name is"+name);
+        System.out.println("my age is"+age);
+    }
+}
