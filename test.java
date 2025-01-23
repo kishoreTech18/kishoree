93d1662030714c5ca76cd04ad35fa31e662c3ece
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-23 19:29:19 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class test{
+    public static void main(String args[])
+    {
+        Scanner kishore = new Scanner(System.in);
+        int a = kishore.nextInt();
+        int b = kishore.nextInt();
+
+        System.out.print(a+b);
+    }
+}
+
