f9524e8e66fae6636a57395c4f7bf5ac5cf7f0bf
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-24 19:04:40 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,25 @@
+class datatype{
+    public static void main(String args[])
+    {
+        byte a = 10;
+        short b = 20;
+        int c = -1;
+        long d = -121234; 
+        float e = 7.15477f;
+        double f = 2.8974325978;
+        char g = 'k';
+        String h = "kishore";
+        boolean i = true;
+        boolean j = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
