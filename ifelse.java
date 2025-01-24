bdcc81e2bd63621c59eb224b620836c13d115c98
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-24 19:40:05 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class ifelse{
+    public static void main(String args[])
+    {
+        //boolean ab = true;
+        //if(ab){
+            //System.out.println("a for apple");
+        
+       // else{
+           // System.out.println("c for cat");
+        
+    //}
+//}
+        //int num1 = 559857234;
+        //int num2 = 587425472;
+
+       // if(num1>num2)
+       // {
+       //     System.out.println("number 1 is greater");
+       // }
+       // else{
+       //     System.out.println("number 2 is greater");
+       // }
+        
+       //int num1 = 444;
+      // int num2 = 404;
+
+      // if(num1==num2)
+      // {
+       // System.out.println("yes, both the number are equal");
+
+       //}
+       //else{
+       // System.out.println("no, both are not equal");
+       //}
+       Scanner kishore = new Scanner(System.in);
+       int a = kishore.nextInt();
+       int b= kishore.nextInt();
+
+       if(a==b)
+       {
+        System.out.println("yes, both are equal");
+       }
+       else{
+        System.out.println("no. both are not same");
+       }
+
+       
+    }
+
+}
