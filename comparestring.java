5af0403a1cda187edac2adf7d9fa9a35f716220d
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-24 21:08:04 +0530

    Add files via upload

---
+++ comparestring.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class comparestring {
+    public static void main(String args[])
+    {
+        //String a ="apple";
+        //String b = "apple";
+
+        
+
+        //String c = new String("apple");
+       // String d = "apple";
+
+        //System.out.println(c.equals(d));
+        //System.out.println(c==d);
+       // String a = "one";
+        //String b = "two";
+        //if(a.equals(b))
+        //{
+        //    System.out.println("both are equals");
+        //}
+        //else{
+        //    System.out.println("both are not equals");
+        //}
+
+        //String a = "one";//abc
+        //String b = "one";//abc
+        //String c = b;//abc
+
+        //System.out.println(a==c);//abc==abc
+
+        //String a = "one";//abc
+        //String b = new String("one");//xyz
+        //String c = b;//xyz
+
+        //System.out.println(a==c);
+       // Scanner sc = new Scanner(System.in);
+        
+        //String RCB = sc.nextLine();
+
+       // if(RCB.equals("panni"))
+       // {
+       //     System.out.println("eesala cup namathey");
+       // }
+       // else{
+       //     System.out.println("cup illa");
+       // }
+       Scanner scan = new Scanner(System.in);
+       String megana = scan.nextLine();
+
+       if(megana.equals("kishore"))
+       {
+        System.out.println("surya meets ramya");
+       }
+       else{
+        System.out.println("surya weds megana");
+       }
+
+
+
+    }
+    
+}
