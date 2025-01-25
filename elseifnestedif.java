04d19f7007a897c1493915bf5b4294729391e4ba
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-25 11:08:21 +0530

    Add files via upload

---
+++ elseifnestedif.java
@@ -0,0 +1,36 @@
+public class elseifnestedif{
+    public static void main(String args[])
+    {
+        //int score = 66;
+        //if(score>35 && score<60 )
+        //{
+        //    System.out.println("hero pen");
+        //}
+        //else if(score>60 && score<90)
+        //{
+        //    System.out.println("remote car");
+        //}
+        //else if(score>90)
+        //{
+        //    System.out.println("cycle");
+        //}
+        boolean temple = true;
+        boolean church = false;
+        boolean mausque = false;
+        if(temple)
+        {
+            System.out.println("murugan");
+
+            if(church)
+            {
+                System.out.println("jesus");
+
+                if(mausque)
+                {
+                    System.out.println("alla");
+                }
+            }
+        }
+    }
+
+}
