6301f5c7347dc9f8a801831b8d54802ceff51346
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-25 17:25:50 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,17 @@
+public class forloop{
+    public static void main(String args[])
+    {
+        //for(int i=1;i<=5;i=i+1)
+
+        //System.out.println("kishore");
+
+        //for(int i=1;i<=10;i=i+1)
+        //System.out.println("kishore");
+
+        //for(int i=1;i<=10;i=i+1)
+        //System.out.println(i);
+
+        for(int i=10;i>=1;i=i-1)
+        System.out.println(i);
+    }
+}
