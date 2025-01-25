42fffc64d8b46a9c434632d8a86922a16dc7609b
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-25 09:41:11 +0530

    Add files via upload

---
+++ codetest.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+public class codetest{
+    public static void main(String args[])
+    {
+        //Scanner kishore = new Scanner(System.in);
+        //int mark = kishore.nextInt();
+        //int mark = 35;
+        //if(mark>34)
+        //{
+        //    System.out.println("you pass");
+        //}
+        //else{
+        //    System.out.println("you fail");
+        //}
+        //int income = 5000;
+        //if(income>7000)
+        //{
+         //   System.out.println("scholarship is available");
+        //}
+        //else{
+        //    System.out.println("not eligible for scholarship");
+        //}
+       // boolean kishore = true;               true && true = true
+       //boolean kumar = true;                  true && false = false
+        //if(kishore && kumar)
+       // {
+        //    System.out.println("get placed");
+       // }
+       // else{
+       //     System.out.println("own company");
+       // }
+      // boolean cricket = false;
+       //boolean football = false;
+       //if(cricket || football)
+       //{
+       // System.out.println("you can play");
+       //}
+       //else{
+       // System.out.println("you cant play");
+       //}
+       //boolean kk = true;
+       //boolean pk = true;
+       //if(kk && pk)
+       //{
+       // System.out.println("it is divisible by both");
+       //}
+       //else{
+       // System.out.println("it is not divisible by both");
+       //}
+       //int num = 10;
+       //System.out.print(15%3);
+       //if(num%3==0)
+       //{
+       // System.out.println("yes");
+       //}
+       //else{
+       // System.out.print("no");
+       //}
+       //int num = 24;
+       //if(num%5==0)
+       //{
+       // System.out.println("yes");
+      // }
+      // else{
+        //System.out.print("no");
+       //}
+       //int num = 15;
+       //if(num%3==0 && num%5==0)
+       //{
+       // System.out.println("divisible by both");
+       //}
+       //else{
+       // System.out.println("not divisible");
+       //}
+       //int num = 5;
+       //if(num%2==0)
+       //{
+       // System.out.println("even");
+       //}
+       //else{
+       // System.out.println("odd");
+      // }
+      //Scanner mama = new Scanner(System.in);
+      //int num = mama.nextInt();
+      //if(num%2==0)
+      //{
+        //System.out.println("even");
+      //}
+      //else{
+        //System.out.println("odd");
+      //}
+      Scanner macha = new Scanner(System.in);
+      int num = macha.nextInt();
+      
+      if(num%3==0 && num%5==0)
+      {
+        System.out.println("yes");
+      }
+      else{
+        System.out.println("no");
+      }
+
+
+
+    }
+}
