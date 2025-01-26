8429fc1e3e06a97665055a9bca74729b8eb7fc50
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-26 12:24:36 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,39 @@
+//import java.util.Scanner;
+public class array{
+    public static void main(String args[])
+    {
+        //String[] gods = new String[5];
+        //gods[0] = "murugaaaaaa";
+        //gods[1] = "shivan";
+        //gods[2] = "kumara";
+        //gods[3] = "vinayagar";
+        //gods[4] = "hanuman";
+        //System.out.println(gods[3]);
+        //System.out.println(gods[0]);
+
+        //String[] kishore = {"kk","kdfh","gudf","jdfug"};
+       // System.out.println(kishore[2]);
+
+       
+       //int[] mark = new int[5];
+       //Scanner kishore = new Scanner(System.in);
+
+       //mark[0] = kishore.nextInt();
+       //mark[1] = kishore.nextInt();
+       //mark[2] = kishore.nextInt();
+       //mark[3] = kishore.nextInt();
+       //mark[4] = kishore.nextInt();
+
+       //System.out.println(mark[1]+mark[2]+mark[3]+mark[4]);
+
+       int[] mark = new int[5];
+
+       mark[0] = 45;
+       mark[1] = 34;
+       mark[2] = 45;
+       mark[3] = 66;
+       mark[4] = 54;
+
+       System.out.println(mark[1]+mark[0]+mark[3]+mark[4]+mark[2]);
+    }
+}
