6532db6b0f306582122e298db0eeb640b5b4acec
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-27 19:19:17 +0530

    Add files via upload

---
+++ nestedforloop.java
@@ -0,0 +1,59 @@
+public class nestedforloop{
+    public static void main(String args[])
+    {
+        //for(int i =1;i<=3;i=i+1)
+        //{
+        //    for(int j = 4;j<=5;j=j+1)
+        //    System.out.println("kishore");
+        //}
+
+        //teacher 1
+        //for(int i = 1;i<=2;i=i+1)
+        //{
+        //    System.out.println("kishore");
+       // }
+        //teacher 2
+       // for(int j = 1;j<=2;j=j+1)
+       // {
+       //     System.out.println("kishore");
+       // }   
+
+
+       //for(int i=1;i<=4;i=i+1)
+       //{
+       // for(int j = 1;j<=3;j=j+1)
+       // System.out.println("kishore");
+       //} 
+
+       //for(int i=1;i<=3;i=i+1)
+       //{
+        //for(int j=1;j<=3;j=j+1)
+        //{
+           // System.out.print("*");
+        //}
+          //  System.out.println();
+       //}
+
+       //for(int i=1;i<=5;i=i+1)
+       //{
+       // for(int j=1;j<=i;j=j+1)
+       // {
+       //     System.out.print("*");
+       // }
+       // System.out.println();
+       //}
+
+       //reverse
+
+       for(int i=10;i>=1;i=i-1)
+       {
+        for(int j=1;j<=i;j=j+1)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+
+
+    }
+}
