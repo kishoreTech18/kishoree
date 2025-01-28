804f397a9e0595dbf9ee7708152f82916bc85431
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-28 18:53:28 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class dowhile{
+    public static void main(String args[])
+    {
+       // int i = 0;
+        //do { 
+        //    System.out.println("kishore");
+        //    i=i+1; 
+        
+       // }while(i<=5); 
+       Scanner scan = new Scanner(System.in);
+       int a=0;
+
+       do{
+        a = scan.nextInt();
+
+       }while(a<10);
+
+    }
+}
