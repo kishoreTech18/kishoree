bc80bb23ecf8cc01722500e9349c81fb952d93db
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-29 19:32:44 +0530

    Add files via upload

---
+++ shop.java
@@ -0,0 +1,54 @@
+//public class shop{
+
+    //void soap(int money)
+    //{
+     //   System.out.println(money);
+     //   System.out.println("soap vangiyachi");
+    //}
+
+    //void chocolate(int kaasu)
+   // {
+     //   System.out.println(kaasu);
+    //    System.out.println("chocolate illa yaaa ");
+   // }
+
+   // void tissue(int dabbu)
+   // {
+   //     System.out.println(dabbu);
+   //     System.out.println("tissue purchased");
+   // }
+
+   // void powder(int panam)
+   // {
+   //     System.out.println(panam);
+   //     System.out.println("enna brand venum");
+   // }
+//    public static void main(String args[])
+   // {
+     //   shop owner = new shop();
+     //   owner.soap(20);
+      //  owner.chocolate(15);
+      //  owner.tissue(40);
+      //  owner.powder(90);
+
+    //}
+//}
+
+public class shop{
+
+    void sum(int num1, int num2)
+    {
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1%num2);
+
+
+    }
+    public static void main(String args[])
+    {
+        shop kishore = new shop();
+        kishore.sum(10,20);
+
+    }
+
+}
