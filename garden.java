d80d67d9f6e1f72ab67d34ff786c3744f90d111f
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-29 19:03:20 +0530

    Add files via upload

---
+++ garden.java
@@ -0,0 +1,107 @@
+//public class functions{
+    //void study()
+    //{
+    //    System.out.println("parama padi daa");
+    //    study2();
+    
+
+    //void study2()
+    //{
+    //    System.out.println("padicha placement");
+    //}
+
+   // public static void main(String args[])
+   // {
+   //     functions kishore = new functions();
+   //     kishore.study();
+       
+    //}
+//}
+            //static function
+   //static  void thala()
+    //{
+     //   System.out.println("ajith kumar");
+     //   functions kishore2 = new functions();
+     //   kishore2.thalapathy();
+        
+    //}
+    
+   // void thalapathy()
+    //{
+    //    System.out.println("vijay");
+    //}
+
+    //public static void main(String args[])
+    //{
+    //    functions cinema = new functions();
+    //    thala();
+
+    //}
+//}
+
+//public class mobile{
+
+    //void iphone()
+    //{
+    //    System.out.println("this is a iphone");
+    //}
+
+    //void oppo()
+    //{
+    //    System.out.println("this is oppo mobile");
+    //}
+
+    //void samsung()
+    //{
+    //    System.out.println("this is samsung");
+    //}
+
+    //void realme()
+    //{
+    //    System.out.println("this is realme mobile");
+    //}
+    //public static void main(String args[])
+    ///{
+    //    mobile shop = new mobile();
+    //    shop.oppo();
+    //    shop.samsung();
+    //    shop.realme();
+    //    shop.iphone();
+
+   // }
+//}
+
+//public class addition{
+    //int a = 10;
+    //int b = 20;
+
+   // void sum()
+    //{
+    //    System.out.println(a+b);
+    //}
+
+    //public static void main(String args[])
+    //{
+    //    addition solution = new addition();
+    //    solution.sum();
+
+   // }
+//}
+
+public class garden{
+
+    int apple_price = 20;
+    int apple_count = 5;
+
+    void total_money()
+    {
+        System.out.println(apple_price*apple_count);
+    }
+
+    public static void main(String args[])
+    {
+        garden pookal = new garden();
+        pookal.total_money();
+
+    }
+}
