5424363cc734714568cfb3bbb0dba944fa687853
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-01-30 19:06:36 +0530

    Add files via upload

---
+++ returnkey.java
@@ -0,0 +1,82 @@
+
+
+    //int  soap(int money)
+    //{
+        //int price = 40;
+        //int balance = 10;
+        //return balance;
+
+
+        //return 100;
+
+    //}
+    //public static void main(String args[])
+    //{
+    //    returnkeyword owner = new returnkeyword();
+    //   int reminder =  owner.soap(50);
+    //   System.out.println(reminder);
+
+   // }
+
+    
+//}
+//public class returnkey{
+
+    //int sum(int a,int b)
+    //{
+    //    int total = a*b;
+    //    return total; 
+    //}
+    //public static void main(String args[])
+    //{
+    //    returnkey amount = new returnkey();
+    //    int total = amount.sum(10,20);
+    //    System.out.println(total);
+
+   // }
+
+  
+//}
+
+//public class returnkey{
+    //String getname()
+    //{
+     //   return "kishore";
+
+    //}
+    //public static void main(String args[])
+   // {
+    //    returnkey name = new returnkey();
+    //    String myname = name.getname();
+    //    System.out.println(myname);
+
+
+    //}
+//}
+
+public class returnkey{
+
+    String name()
+    {
+        return "kishore";
+    }
+    String phone()
+    {
+        return "oppo";
+    }
+    String address()
+    {
+        return"chennai";
+    }
+    public static void main(String args[])
+    {
+        returnkey obj1 = new returnkey();
+        String name = obj1.name();
+        System.out.println(name);
+        String phone = obj1.phone();
+        System.out.println(phone);
+        String address = obj1.address();
+        System.out.println(address);
+
+    }
+}
