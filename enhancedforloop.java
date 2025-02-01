e1dafecdeffa29ba5ca6d4a127934b9e02340297
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-02-01 09:32:39 +0530

    Add files via upload

---
+++ enhancedforloop.java
@@ -0,0 +1,21 @@
+public class enhancedforloop{
+    public static void main(String args[])
+    {
+        //int arr[] = {12,33,44,556,6};
+        //for(int i = 0;i<5;i=i+1)
+        //{
+        //    System.out.println(arr[i]);
+        //}
+
+       // for(int i:arr)
+        //{
+        ///    System.out.println(i);
+        //}
+
+        String arr[] = {"kishore","oru","loosu"};
+        for(String i : arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
