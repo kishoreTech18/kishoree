58acee31f69418b78efa96d3f46600196f684d1b
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-02-04 19:38:53 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,30 @@
+//class phone {
+	//String name = "oppo";
+//}
+
+//class car{
+//	int price;
+//}
+
+class kpi{
+	int turnover = 1000000000;
+}
+
+class kit extends kpi{
+	
+}
+
+public class inheritance {
+
+	public static void main(String[] args) {
+		//phone ph = new phone();
+		//System.out.println(ph.name);
+		//car ca = new car();
+		//ca.price=1000;
+		//System.out.println(ca.price);
+		kpi ravi =  new kpi();
+		System.out.println(ravi.turnover);
+
+	}
+
+}
