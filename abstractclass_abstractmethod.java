04f2566084ffeaaa4d4fedc2ba3f811645207cbd
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-02-06 19:37:09 +0530

    Add files via upload

---
+++ abstractclass_abstractmethod.java
@@ -0,0 +1,32 @@
+abstract class vehicle{
+	abstract void speed();
+	
+}
+
+class bike extends vehicle{
+	void speed()
+	{
+		System.out.println("114/hr");
+	}
+}
+class car extends vehicle{
+	void speed()
+	{
+		System.out.println("223/hr");
+	}
+}
+class truck extends vehicle{
+	void speed()
+	{
+		System.out.println("445/hr");
+	}
+}
+
+public class abstractclass_abstractmethod {
+
+	public static void main(String args[]) {
+		
+	
+	}
+
+}
