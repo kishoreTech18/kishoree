bbd7e55b97dc16c4bcd9cc94b94925777b19e441
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-02-09 17:11:42 +0530

    Add files via upload

---
+++ compareproblem.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class compareproblem {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num>=90)
+        {
+            System.out.println("A");
+        }
+        else if(num>80 && num<89)
+        {
+            System.out.println("B");
+        }
+        else if(num>70 && num<79)
+        {
+            System.out.println("C");
+        }
+        else if(num>60 && num<69)
+        {
+            System.out.println("D");
+        }
+        else{
+            System.out.println("F");
+        }
+    }
+    
+}
