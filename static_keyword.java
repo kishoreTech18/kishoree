8e4500fd16ec483c52a30c992a0181a54d181d43
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-02-13 19:26:11 +0530

    Add files via upload

---
+++ static_keyword.java
@@ -0,0 +1,41 @@
+/*class teacher{
+	static String name = "kishore";
+	
+	static void display()
+	{
+		System.out.println("all the best");
+	}
+}*/
+public class static_keyword {
+	
+	
+	
+	//static int num = 100;
+
+	
+	static {
+		System.out.println("yes bro");
+	}
+	
+	public static void main(String[] args) {
+		/*teacher t1 = new teacher();
+		teacher.name = "kumar";
+		System.out.println(t1.name);
+		
+		teacher.display();*/
+		
+		
+		
+		
+		//System.out.println(num);
+		
+		
+		System.out.println("hello");
+
+		static_keyword key = new static_keyword();
+		static_keyword key1 = new static_keyword();
+		
+		
+	}
+
+}
