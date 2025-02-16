f9a23b26e69505369af6f635beb2b4eb7b7b4035
kishoreTech18 <vh11946_ai22@velhightech.com>
2025-02-16 15:03:38 +0530

    Add files via upload

---
+++ finalkeyword.java
@@ -0,0 +1,31 @@
+
+class a{
+    final void display()
+     {
+         System.out.println("hello");
+     }
+ }
+ 
+ class b extends a{
+     
+ }
+ public class finalkeyword {
+     
+ 
+     public static void main(String[] args) {
+         //final
+         /* final int a = 10;
+     
+         System.out.println(a);*/
+         
+         ///////////////////////////////////////
+         
+         b obj1 = new b();
+         obj1.display();
+     
+         
+         
+     }
+     
+ 
+ }
